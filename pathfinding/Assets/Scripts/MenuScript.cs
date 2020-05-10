018f40313711f753346375d5009b79e5eb3f2226
Rasputin <medina.engineering@outlook.com>
2020-05-10 05:30:47 -0400

    Setup

---
+++ pathfinding/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuScript 
+{
+    [MenuItem("Tools/Assign Tile Material")]
+    public static void AssignTileMaterial()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        Material mat = Resources.Load<Material>("Material/Tile");
+
+        foreach (var item in tiles)
+        {
+            item.GetComponent<Renderer>().material = mat;
+        }
+    }
+
+    [MenuItem("Tools/Assign Tile Script")]
+    public static void AssignTileScript()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach (var item in tiles)
+        {
+            item.AddComponent<Tile>();
+        }
+    }
+
+}
